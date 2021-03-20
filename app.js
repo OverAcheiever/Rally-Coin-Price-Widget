b0bc93076d128910c38d2e6ae40e9f3f670b61f9
OverAcheiever <keshava.reddy.100580@standrewsindia.com>
2021-03-21 00:53:19 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-let coin = "SKOT";
+let fieldData = {};
 let old_price = 0;
+let coin = "";
+let color = '';
+window.addEventListener("onWidgetLoad", function (obj) {
+  let fieldData = obj["detail"]["fieldData"];
+  coin = fieldData.creatorCoin;
+  color = fieldData.borderColor;
+  document.querySelector(".coin-name").innerHTML = coin;
+  if (fieldData.imageSource == "custom" && fieldData.customImage !== "") {
+    document.getElementById(
+      "coin-image"
+    ).style.backgroundImage = `url("${fieldData.customImage}")`;
+  } else {
+    Http = new XMLHttpRequest();
+
+    Http.onreadystatechange = function () {
+      if (Http.readyState == 4 && Http.status == 200) {
+        let list = JSON.parse(Http.responseText);
+        for (obj in list) {
+          if (list[obj].coinSymbol == coin) {
+            document.getElementById(
+              "coin-image"
+            ).style.backgroundImage = `url(${list[obj].coinImagePath})`;
+            console.log(list[obj].coinImagePath);
+          }
+        }
+      }
+    };
+
+    Http.open("GET", `https://api.rally.io/v1/creator_coins`);
+    Http.send(null);
+  }
+});
 
 function getPrice() {
   Http = new XMLHttpRequest();
@@ -8,18 +40,19 @@ function getPrice() {
     if (Http.readyState == 4 && Http.status == 200) {
       new_price = JSON.parse(Http.responseText).priceInUSD;
       console.log(new_price);
-      document.querySelector(".price").innerHTML = "$" + new_price.toFixed(2);
+      document.querySelector(".price").innerHTML =
+        "$" + new_price.toFixed({decimalPlaces});
       if (new_price > old_price) {
         anime({
           targets: ".background",
           keyframes: [
             { backgroundColor: "#06bf3d" },
-            { backgroundColor: "#333", delay: 500 },
+            { backgroundColor: color , delay: 500 },
           ],
         });
         anime({
           targets: ".price",
-          keyframes: [{ color: "#06bf3d" }, { color: "#333", delay: 500 }],
+          keyframes: [{ color: "#06bf3d" }, { color:  `#333`, delay: 500 }],
         });
       }
       if (old_price > new_price) {
@@ -27,12 +60,12 @@ function getPrice() {
           targets: ".background",
           keyframes: [
             { backgroundColor: "#ff3636" },
-            { backgroundColor: "#333", delay: 500 },
+            { backgroundColor: color, delay: 500 },
           ],
         });
         anime({
           targets: ".price",
-          keyframes: [{ color: "#ff3636" }, { color: "#333", delay: 500 }],
+          keyframes: [{ color: "#ff3636" }, { color: `#333`, delay: 500 }],
         });
       }
 
